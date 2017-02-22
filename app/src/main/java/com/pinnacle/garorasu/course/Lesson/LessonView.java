f92a2cdf966ac8494ac1b67ff1f5b0ec33055dd9
Rumani <rumanicharaya491@gmail.com>
2017-02-22 15:16:41 -0800

    Initial commit

---
+++ app/src/main/java/com/pinnacle/garorasu/course/Lesson/LessonView.java
@@ -0,0 +1,14 @@
+package com.pinnacle.garorasu.course.Lesson;
+
+import com.pinnacle.garorasu.course.Explore.Subject;
+
+/**
+ * Created by Ideal on 2/18/2017.
+ */
+
+public interface LessonView {
+    void onLessonClick(Lesson lesson);
+    void showProgress();
+    void hideProgress();
+
+}
